a20c4ab046ff5a739a5ec7a535415601d927fbc2
Davi <davi@MBP-de-Diana.lan>
2023-08-18 20:02:15 -0300

    first upload complete project of a todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import styles from './app.module.css'
+import Taskform from './components/taskform'
+import TaskList from './components/tasklist';
+import { ITask } from './interfaces/task';
+import Modal from './components/modal';
+
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([])
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null)
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
+    const editTask = (task: ITask): void => {
+    hideOrShowModal(true);  
+    setTaskToUpdate(task);
+  }
+  
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updateTask: ITask = {id, title, difficulty}
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updateTask.id ? updateTask : task
+    })
+
+    setTaskList(updatedItems)
+
+    hideOrShowModal(false)
+
+  }
+
+
+  const hideOrShowModal = (display: boolean) => {
+  const modal = document.querySelector("#modal")
+  if(display){
+      modal!.classList.remove("hide")
+    } else {
+      modal!.classList.add("hide")
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      
+    <Modal 
+    children={
+    <Taskform 
+    btnText='Edit task' 
+    taskList={taskList} 
+    task={taskToUpdate} 
+    handleUpdate={updateTask}
+    />
+    }
+    />
+    <Header />
+    <main 
+    className={styles.main}
+    >
+    <div>
+      <h2>What are you going to do?</h2>
+      <Taskform 
+      btnText='Create a task' 
+      taskList={taskList} 
+      setTaskList={setTaskList}
+      />
     </div>
-  );
+    
+    <div>
+      <h2>Your taks:</h2>
+      <TaskList 
+      taskList={taskList} 
+      handleDelete={deleteTask} 
+      handleEdit={editTask} />
+    </div>
+    </main>
+    <Footer />
+  </div>
+
+)
+  
 }
 
 export default App;
---
+++ src/components/tasklist.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ITask } from "../interfaces/task";
+
+import styles from './tasklist.module.css'
+
+interface Props {
+    taskList: ITask[]
+    handleDelete(id: number): void
+    handleEdit(task: ITask): void;
+}
+
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
+    return (
+        <>
+        {taskList.length > 0 ? (
+            taskList.map((task) => (
+                <div key={task.id} className={styles.task}>
+                    <div className={styles.details}> 
+                        <h4>{task.title}</h4>
+                        <p>Difficulty: {task.difficulty}</p>
+                    </div>
+                    <div className={styles.actions}>
+                        <i className="bi bi-pencil" onClick={() => { handleEdit(task)}}></i>
+                        <i 
+                        className="bi bi-trash" 
+                        onClick={() => {
+                            handleDelete(task.id);
+                            }}
+                            ></i>
+                    </div>
+                </div>
+            ))
+        ) : (
+            <p>There are no tasks registered</p>
+        )}
+        </>
+    )
+}
+
+export default TaskList
